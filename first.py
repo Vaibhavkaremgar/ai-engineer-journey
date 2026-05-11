115afa6668d96290f813b6ee2924a692f1c9f516
VaibhavKaremgar <karemgarvaibhav@gmail.com>
2026-05-11 23:17:38 +0530

    Day 1 : python basics0

---
+++ first.py
@@ -0,0 +1,4 @@
+name = input("enter your name: ")
+song = input("enter your favorite song: ")
+dream_company = input("enter your dream company: ")
+print(f"my name is {name} my favorite song is {song} and and i want to work in a well structured company like {dream_company}")
